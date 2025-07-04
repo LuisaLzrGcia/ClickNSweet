f76e86a55e73ea73cb571a6b835b8da4fc7f01df
LuisaLzrGcia <maluisa.lzrgcia@gmail.com>
2025-07-04 12:48:51 -0600

    correccion de responsividad en el carrusel de categorias en index

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
+import { loadCategoryCassurel } from "./functions/loadCategoryCassurel.js";
 document.addEventListener("DOMContentLoaded", () => {
-      new Swiper(".mySwiper", {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        loop: true,
-        autoplay: {
-          delay: 3000,
-          disableOnInteraction: false,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        breakpoints: {
-          1024: { slidesPerView: 4 },
-          768: { slidesPerView: 3 },
-          480: { slidesPerView: 2 },
-          0: { slidesPerView: 1 },
-        },
-      });
-    });
+  loadCategoryCassurel()
+});
+
