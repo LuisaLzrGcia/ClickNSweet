c0c9ec3a790d62d2170a639843d2dbfe45a57307
Omar <omaraguilalira2@gmail.com>
2025-07-11 10:12:13 -0600

    modificaciones para administrador

--- functions/updateNavbarAuthState.js
+++ functions/updateNavbarAuthState.js
@@ -1,11 +1,22 @@
 import { getCurrentUser } from "../login/auth.js";
 
 const autenticacionLinks = document.querySelectorAll(".autenticacion");
+const navLinkAdmin = document.querySelectorAll(".nav-item-admin");
+const navLinkUser = document.querySelectorAll(".nav-item-user");
 const userMenu = document.querySelector(".user-menu");
 
 export function updateNavbarAuthState() {
   const currentUser = getCurrentUser();
   if (currentUser) {
+    if (currentUser.role === "admin") {
+      navLinkAdmin.forEach((navLink) => {
+        navLink.classList.remove("d-none");
+      });
+      navLinkUser.forEach((navLink) => {
+        navLink.classList.add("d-none");
+        console.log(navLink);
+      });
+    }
     autenticacionLinks.forEach((el) => el.classList.add("d-none"));
     userMenu.classList.remove("d-none");
     console.log("Usuario logueado:", currentUser);
