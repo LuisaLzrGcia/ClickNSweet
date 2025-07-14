bba46ea180635fad9d770ca86d2c5313757a2dea
Omar <omaraguilalira2@gmail.com>
2025-07-13 21:31:55 -0600

    actualizaciones barra de navegación

--- main.js
+++ main.js
@@ -2,16 +2,14 @@ import { displayActiveLink } from "./functions/displayActiveLink.js";
 import { preventLoginIfAuthenticated } from "./functions/preventLoginIfAuthenticated.js";
 import { updateNavbarAuthState } from "./functions/updateNavbarAuthState.js";
 import { logout } from "./login/auth.js";
-
+const navbarLinks = document.querySelector(".navbar-nav");
 const logoutButton = document.getElementById("logout");
 
 document.addEventListener("DOMContentLoaded", () => {
-  displayActiveLink()
-  updateNavbarAuthState()
-  preventLoginIfAuthenticated()
-  window.addEventListener('hashchange', () => displayActiveLink());
-
+  displayActiveLink();
+  updateNavbarAuthState();
+  preventLoginIfAuthenticated();
+  window.addEventListener("hashchange", () => displayActiveLink());
+  navbarLinks.classList.remove("invisible");
+  logoutButton.addEventListener("click", () => logout());
 });
-
-  logoutButton.addEventListener("click", () => logout())
-
