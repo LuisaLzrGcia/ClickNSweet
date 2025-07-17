9a4332a2f94087a761361b32e276c0e44da0f993
Omar <omaraguilalira2@gmail.com>
2025-07-17 16:48:07 -0600

    actualización indicador compras

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import { displayActiveLink } from "./functions/displayActiveLink.js";
 import { preventLoginIfAuthenticated } from "./functions/preventLoginIfAuthenticated.js";
+import { loadCartCount } from "./functions/loadCartCount.js";
 import { updateNavbarAuthState } from "./functions/updateNavbarAuthState.js";
 import { logout } from "./login/auth.js";
+
 const navbarLinks = document.querySelector(".navbar-nav");
 const logoutButton = document.getElementById("logout");
 
 document.addEventListener("DOMContentLoaded", () => {
+  loadCartCount()
   displayActiveLink();
   updateNavbarAuthState();
   preventLoginIfAuthenticated();
