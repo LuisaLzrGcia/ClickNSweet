a5795ff0d68f80744a3c311d41e9709cb6f9d401
Omar <omaraguilalira2@gmail.com>
2025-07-28 13:04:40 -0600

    correcciones

--- functions/preventLoginIfAuthenticated.js
+++ functions/preventLoginIfAuthenticated.js
@@ -6,7 +6,7 @@ const isInLoginPage = currentPath.includes("login");
 export function preventLoginIfAuthenticated() {
   const currentUser = getCurrentUser();
   if (currentUser && isInLoginPage) {
-    const homePage = currentUser.role === "admin" ? "/main-manage-products" : "/index.html";
+    const homePage = currentUser.role === "admin" ? "main-manage-products" : "index.html";
     window.location.href = homePage;
   }
 }
